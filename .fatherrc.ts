20a0209be98b42d9940283203dc9154a6ce0ec7b
mCloud <349579044@qq.com>
2020-09-03 13:10:16 +0800

    修改

--- .fatherrc.ts
+++
@@ -1,8 +0,0 @@
-import url from '@rollup/plugin-url';
-
-export default {
-  entry: 'src/index.ts',
-  esm: 'rollup',
-  cssModules: true,
-  extraRollupPlugins: [],
-}
--- src/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Preview } from './components/preview';
-export { default as Drawer } from './components/drawer';
-export { default as Report } from './components/report';
-
